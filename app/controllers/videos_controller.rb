a0704aab7e313528931ecfa0a999cbe773be4654
Shivam Mahajan <pro.dev.shivay@gmail.com>
2024-10-09 14:57:12 +0530

    add some restriction

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,12 @@
 class VideosController < ApplicationController
 
+
     def show
         @video = Video.find(params[:id])
+        if !(current_user.courses.include?(@video.course))
+            flash[:notice] = "First Enroll to that Course"
+            redirect_to root_path
+        end
     end
 
 end
